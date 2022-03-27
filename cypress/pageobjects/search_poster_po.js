1bbcdcefc94aa182e59e4cc7d3b2aec45187a652
spidergirl2181 <97619215+spidergirl2181@users.noreply.github.com>
2022-03-27 09:25:10 +0700

    Add search_poster_po.js

---
+++ cypress/pageobjects/search_poster_po.js
@@ -0,0 +1,28 @@
+const posterBtn = 'Posters'
+const searchPosterBox = '[id=q]'
+const 
+
+export const searchPoster = {
+	launchPosterPage () {
+		cy.get(posterBtn).click()
+	}
+	searchBoxAction (args) {
+		cy.get(searchPosterBox)
+			.click()
+			.clear()
+			.type(args)
+	}
+
+	searchPoster () {
+		cy.fixture('testdata').then((data) => {
+      		this.searchBoxAction(data.searchkeyword)
+      		cy.wait(2000)
+    	}		
+	}
+
+  	verifyPosterDisplayed () {
+  		cy.fixture('testdata').then((data) => {
+  			cy.get(data.searchresult).should('exist')
+    	}	
+    }
+}
